4f9749fff661afb1ea72f6c63701664b5d3d0341
zzw <2044968491@qq.com>
2022-12-19 15:27:52 +0800

    second

--- app/src/main/java/com/wxy3265/vmeknowledge/MainActivity.kt
+++ app/src/main/java/com/wxy3265/vmeknowledge/MainActivity.kt
@@ -9,5 +9,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.d("Good", "onCreate: wxy3265")
+        Log.d("Good", "onCreate: ZWZWW")
     }
 }
