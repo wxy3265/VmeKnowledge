8e6a14f076df6d78618096c4e9d7e18650ce7d1b
wxy3265 <546668033@qq.com>
2022-12-19 15:17:35 +0800

    Test Commit wxy3265

--- app/src/main/java/com/wxy3265/vmeknowledge/MainActivity.kt
+++ app/src/main/java/com/wxy3265/vmeknowledge/MainActivity.kt
@@ -2,10 +2,12 @@ package com.wxy3265.vmeknowledge
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("Good", "onCreate: wxy3265")
     }
 }
