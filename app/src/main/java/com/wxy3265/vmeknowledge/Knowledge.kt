a5a2e2a79211bd20ff96347378f8bf2bf0632ddb
zzs <3522059578@qq.com>
2023-01-13 21:46:49 +0800

    改了个报错

--- app/src/main/java/com/wxy3265/vmeknowledge/Knowledge.kt
+++ app/src/main/java/com/wxy3265/vmeknowledge/Knowledge.kt
@@ -1,4 +1,4 @@
 package com.wxy3265.vmeknowledge
 
-class Knowledge(val Content: String, val CreateDate: String, val ReviewDate: String, val Id: Int, val StudyTimes: Int, val MilliTime: Int) {
+class Knowledge(val Content: String, val Date: String,val reviewdate:String,val Id: Int, val StudyTimes: Int, val MilliTime: Int) {
 }
