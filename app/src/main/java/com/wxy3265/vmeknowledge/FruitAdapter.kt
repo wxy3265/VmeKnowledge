14cdb0939212b0d16b7bb72e8bb358116e71a270
rst <1318303227@qq.com>
2023-01-16 22:08:39 +0800

    增添了标签点击功能的雏形

--- app/src/main/java/com/wxy3265/vmeknowledge/FruitAdapter.kt
+++ app/src/main/java/com/wxy3265/vmeknowledge/FruitAdapter.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 
 class FruitAdapter (val fruitList: List<Fruit>):RecyclerView.Adapter<FruitAdapter.ViewHolder>(){
@@ -14,7 +15,13 @@ class FruitAdapter (val fruitList: List<Fruit>):RecyclerView.Adapter<FruitAdapte
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view=LayoutInflater.from(parent.context).inflate(R.layout.fruit_item,parent,false)
-        return ViewHolder(view)
+        val viewHolder=ViewHolder(view)
+        viewHolder.itemView.setOnClickListener {
+            val position=viewHolder.adapterPosition
+            val fruit=fruitList[position]
+            Toast.makeText(parent.context,"you click ${fruit.name}",Toast.LENGTH_SHORT).show()
+        }
+        return viewHolder
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
