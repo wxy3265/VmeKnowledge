92a8508e8000504dc9f0d510242f38095857a600
rst <1318303227@qq.com>
2023-01-29 18:07:26 +0800

    做好了日程管理除了recyclerview的部分

--- app/src/main/java/com/wxy3265/vmeknowledge/richengguanlimainactivity.kt
+++ app/src/main/java/com/wxy3265/vmeknowledge/richengguanlimainactivity.kt
@@ -3,15 +3,36 @@ package com.wxy3265.vmeknowledge
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_richengguanlimainactivity.*
 import kotlinx.android.synthetic.main.menu.*
 
 class richengguanlimainactivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_richengguanlimainactivity)
+        setSupportActionBar(richengguanliToolbar)
         menumain.setOnClickListener {
             val intent_main = Intent(this , MainActivity::class.java)
             startActivity(intent_main)
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar2,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.richengguanliAdd -> startAdd2()
+        }
+        return true
+    }
+    private fun startAdd2()
+    {
+        val intentstartAdd = Intent(this , richengtianjia::class.java)
+        startActivity(intentstartAdd)
     }
 }
